df3ecd7bb79b8d70234b385af18a6743be73d57c
ulises <ulises.lch1@gmail.com>
2017-03-10 11:52:22 -0500

    alumno

--- Project/script.js
+++ Project/script.js
@@ -1,5 +1,9 @@
 var app=angular.module("app",[]);
  
-function PruebaController($scope) {
-  $scope.mensaje="Hola Mundo";
+function AlumnosController($scope) {
+  $scope.alumnos=[
+{nombre:"elbio labra",telefono:"983811310",curso:"segundo eso"}
+
+
+  ];
 }
